421b87bee3e88766282cdce48001b602d131becb
Victor Goudal <vi.goudal@gmail.com>
2024-11-06 16:41:14 -0500

    get better plot + save dfs

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 import matplotlib.pyplot as plt
 import glob
@@ -9,7 +11,8 @@ from object_segmentation import ObjectSegmentation
 def main():
 
     # Initialize figure for plotting
-    fig, axes = plt.subplots(nrows=3, ncols=6, figsize=(12, 10))
+    fig, axes = plt.subplots(nrows=3, ncols=6, figsize=(12, 10),
+                        gridspec_kw=dict(width_ratios=[1,0.5, 1,0.5, 1,0.5]))
     fig.subplots_adjust(hspace=0.4, wspace=0.4)  # Adjust spacing
 
     # Create the object segmentation class
@@ -18,40 +21,46 @@ def main():
     # Load the images from "Images" folder
     paths_objs = glob.glob('Images/Echantillion1Mod2_*.*')
 
+    # Folder path
+    folder_path = 'dataframes'
+
+    # Check if the folder exists, if not, create it
+    if not os.path.exists(folder_path):
+        os.makedirs(folder_path)
+        print(f"Folder '{folder_path}' created.")
+    else:
+        print(f"Folder '{folder_path}' already exists.")
+
     for idx, (ax, path_obj) in enumerate(zip(axes.flat, paths_objs)):
+
+        # Get the file name
+        file_name = os.path.splitext(os.path.basename(path_obj))[0]
+
         # Load the image
         img_colored = cv2.imread(path_obj)
 
         # Segment the image
         segmented_image, df = obj_seg.segment(img_colored)
 
-        # Resize from 600 600 to 300 300
-        img_colored = cv2.resize(img_colored, (300, 300))
+        # Save the dataframe to a CSV file
+        df.to_csv(f"{folder_path}/{file_name}.csv", index=False)
 
         row = idx // 3
         col = (idx % 3) * 2
-        # Display original image
-        axes[row, col].imshow(img_colored, extent=(0, 0.5, 0, 0.5))
-        axes[row, col].axis('off')  # Hide axis for cleaner display
+
         # Display processed image
-        axes[row, col + 1].imshow(segmented_image, extent=(0, 1, 0, 1))
+        axes[row, col].imshow(segmented_image, vmin=0, vmax=1)
+        axes[row, col].set_title(file_name)
+        axes[row, col].axis('off')  # Hide axis for cleaner display
+
+        # Display original image
+        axes[row, col + 1].imshow(img_colored, vmin=0, vmax=1)
         axes[row, col + 1].axis('off')  # Hide axis for cleaner display
+        
 
     # Adjust layout and display the plot
     plt.tight_layout()
     plt.show()
-
-    
-
-    # Display each image
-    # for i, ax in enumerate(axes.flat):
-    #     ax.imshow(images_rgb[i])
-    #     ax.axis('off')  # Hide axis for cleaner display
-
-    # Display the image
-    # cv2.imshow("Segmented Image", segmented_image)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
--- object_segmentation.py
+++ object_segmentation.py
@@ -1,3 +1,5 @@
+import os
+
 import cv2
 from skimage.filters import sobel
 from skimage.segmentation import watershed
@@ -22,13 +24,6 @@ class ObjectSegmentation:
         # Threshold the image
         edges = sobel(image)
 
-        segmented_image, df = self.draw_image(image, edges, rgb_image)
-
-        return segmented_image, df
-    
-    # Draw borders around the objects
-    def draw_image(self, image, edges, rgb_image):
-
         # Add markers
         markers = np.zeros_like(image)
         foreground, background = 1, 2
@@ -38,18 +33,27 @@ class ObjectSegmentation:
         # Apply watershed algorithm
         ws = watershed(edges, markers)
         seg = label(ws == foreground)
-        data = []
+
+        segmented_image = self.draw_image(seg, rgb_image)
+
+        df = self.get_dataframes(seg, rgb_image)
+
+        return segmented_image, df
+    
+    # Draw borders around the objects
+    def draw_image(self, seg, rgb_image):
 
         # Create a copy to draw the contours and numbers
         img_contours = rgb_image.copy()
-        img_contours_blurred = rgb_image.copy()
-
         # Trouver les contours de chaque segment
         contours, _ = cv2.findContours(seg.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # Dessiner les contours et numéroter les régions
         for i, contour in enumerate(contours):
+
+            # Dessiner le contour
             cv2.drawContours(img_contours, [contour], -1, (0, 255, 0), 2)  # Dessiner le contour en vert
+
             # Calculer le centre du contour pour positionner le numéro
             M = cv2.moments(contour)
             if M["m00"] != 0:
@@ -58,7 +62,19 @@ class ObjectSegmentation:
                 # Ajouter le numéro de l'étiquette
                 cv2.putText(img_contours, str(i + 1), (cX, cY),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)  # Numéro bleu
-                
+
+        return img_contours
+    
+    def get_dataframes(self, seg, rgb_image):
+
+        data = []
+
+        # Trouver les contours de chaque segment
+        contours, _ = cv2.findContours(seg.astype(np.uint8), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Dessiner les contours et numéroter les régions
+        for i, contour in enumerate(contours):
+
             mask = np.zeros(rgb_image.shape[:2], dtype=np.uint8)
             cv2.drawContours(mask, [contour], -1, 255, -1)
 
@@ -68,6 +84,7 @@ class ObjectSegmentation:
 
             data.append([i + 1, b_mean, g_mean, r_mean])
 
-            df = pd.DataFrame(data, columns=["Numéro", "B_mean", "G_mean", "R_mean"])
+        # Create a dataframe from data
+        df = pd.DataFrame(data, columns=["Numéro", "B_mean", "G_mean", "R_mean"])
 
-        return img_contours,  df
+        return df
